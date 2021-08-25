2095ff2e389987e527ffb668d348615e0f4bd8d0
Guilherme Monteiro <corvonauta@outlook.com>
2021-08-25 16:07:36 -0300

    Completo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Aprendizagem-de-Maquina
 Aplicação de algoritmos de aprendizagem de máquina no game Sapace Invaders de ATARI, para fins de estudos e agregação de conhecimento.
+
+Foi selecionado uma aplicação em jogos, mais especificamente ao famoso jogo “Space Invaders” de 1978, para a plataforma de vídeo games Atari, aplicado ao jogo foram utilizados dois algoritmos de aprendizagem por reforço, Deep Q-Networks e Cross-Entropy Method, a fim de otimizar a quantidade de pontos obtidos em uma partida com 3 tentativas.
+
+
+Para a compilação e execução do programa é preciso:
+- Executar o comando
+  pip install tensorflow==2.5.0 gym keras-rl2 gym[atari]
+- Instalar o emulador Stella
+- Colar a dll ale_c.dll na pasta C:\Python39\Lib\site-packages\atari_py\ale_interface
+- Executar o comando
+  pip install gym[all]
--- space.py
+++ space.py
@@ -8,8 +8,10 @@ from tensorflow.keras.layers import Dense, Flatten, Convolution2D
 from tensorflow.keras.optimizers import Adam
 from rl.agents import DQNAgent
 from rl.agents import CEMAgent
-from rl.memory import SequentialMemory
+
+from rl.memory import EpisodeParameterMemory, SequentialMemory
 from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+from tensorflow.python.keras.layers.core import Activation
 
 def build_model(height, width, channels, actions):
     model = Sequential()
@@ -22,6 +24,13 @@ def build_model(height, width, channels, actions):
     model.add(Dense(actions, activation='linear'))
     return model
 
+def build_model_cem(height, width, channels, actions):
+    model = Sequential()
+    model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+    model.add(Dense(actions))
+    model.add(Activation('softmax'))
+    return model
+ 
 def build_agent(model, actions):
     policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
     memory = SequentialMemory(limit=1000, window_length=3)
@@ -33,8 +42,8 @@ def build_agent(model, actions):
 
 def build_agent_cem(model, actions):
     policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
-    memory = SequentialMemory(limit=1000, window_length=3)
-    cema = CEMAgent(model=model,memory=memory, nb_actions=actions, nb_steps_warmup=1000000)
+    memory = EpisodeParameterMemory(limit=1000, window_length=1)
+    cema = CEMAgent(model=model,memory=memory, nb_actions=actions, nb_steps_warmup=1000000, train_interval=10000)
 
     return cema
 
@@ -44,63 +53,72 @@ actions = env.action_space.n
 
 env.unwrapped.get_action_meanings()
 
-a = open('score_aleatorio.csv','w')
+# a = open('score_aleatorio.csv','w')
 
     
 
-episodes = 2
-for episode in range(1, episodes+1):
-    state = env.reset()
-    done = False
-    score = 0 
+# episodes = 2
+# for episode in range(1, episodes+1):
+#     state = env.reset()
+#     done = False
+#     score = 0 
     
-    while not done:
-        env.render()
-        action = random.choice([0,1,2,3,4,5])
-        n_state, reward, done, info = env.step(action)
-        score+=reward
-    print('Episode:{} Score:{}'.format(episode, score))
-    a.write(str(score))
-    a.write('\n')
-env.close()
-a.close()
+#     while not done:
+#         env.render()
+#         action = random.choice([0,1,2,3,4,5])
+#         n_state, reward, done, info = env.step(action)
+#         score+=reward
+#     print('Episode:{} Score:{}'.format(episode, score))
+#     a.write(str(score))
+#     a.write('\n')
+# env.close()
+# a.close()
 
 
-model = build_model(height, width, channels, actions)
-model.summary()
+# model = build_model(height, width, channels, actions)
+# model.summary()
 
-dqn = build_agent(model, actions)
-dqn.compile(Adam(lr=1e-4))
-# dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
+# dqn = build_agent(model, actions)
+# dqn.compile(Adam(lr=1e-4))
+# s = dqn.fit(env, nb_steps=100000, visualize=True, verbose=2)
+# f = open('score_dqn_train.csv','w')
+# for i in s.history['episode_reward']:
+#     f.write(str(i))
+#     f.write('\n')
+# f.close()
 
 
+# dqn.save_weights('SavedWeights/dqn/dqn_weights.h5f')
+# dqn.load_weights('SavedWeights/dqn/dqn_weights.h5f')
 
-dqn.save_weights('SavedWeights/dqn/dqn_weights.h5f')
-dqn.load_weights('SavedWeights/dqn/dqn_weights.h5f')
+# scores = dqn.test(env, nb_episodes=50, visualize=True)
+# print(scores.history['episode_reward'])
+# f = open('score_dqn_test.csv','w')
+# for i in scores.history['episode_reward']:
+#     f.write(str(i))
+#     f.write('\n')
+# f.close()
 
-scores = dqn.test(env, nb_episodes=2, visualize=True)
-print(scores.history['episode_reward'])
-f = open('score_dqn.csv','w')
-for i in scores.history['episode_reward']:
-    f.write(str(i))
-    f.write('\n')
-f.close()
+# del model
 
-del model
+model = build_model_cem(height, width, channels, actions)
 
-model = build_model(height, width, channels, actions)
-model.summary()
 
 cema = build_agent_cem(model, actions)
 cema.compile()
-# dqn.fit(env, nb_steps=10000, visualize=False, verbose=2)
+# cema.fit(env, nb_steps=10000, visualize=True)
 
+# f = open('score_dqn_train.csv','w')
+# for i in s.history['episode_reward']:
+#     f.write(str(i))
+#     f.write('\n')
+# f.close()
 
 
 cema.save_weights('SavedWeights/cem/dqn_weights.h5f')
 cema.load_weights('SavedWeights/cem/dqn_weights.h5f')
 
-scores = cema.test(env, nb_episodes=2, visualize=True)
+scores = cema.test(env, nb_episodes=50, visualize=True, verbose=2)
 print(scores.history['episode_reward'])
 f = open('score_cem.csv','w')
 for i in scores.history['episode_reward']:
